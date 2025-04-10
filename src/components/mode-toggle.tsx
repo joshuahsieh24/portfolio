cc46deddb3ecf911b8dc323ebca4121044ca0e01
joshuahsieh24 <162934632+josh-hsiehh@users.noreply.github.com>
2025-04-10 14:52:21 -0700

    new netlify fixes

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,12 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
+import { forwardRef } from "react";
 
-export function ModeToggle() {
+export const ModeToggle = forwardRef<HTMLButtonElement>((props, ref) => {
   const { theme, setTheme } = useTheme();
 
   return (
     <Button
+      ref={ref}
       variant="ghost"
       type="button"
       size="icon"
@@ -19,4 +21,6 @@ export function ModeToggle() {
       <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
     </Button>
   );
-}
+});
+
+ModeToggle.displayName = "ModeToggle";
